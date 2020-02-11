3f1a5b4787014e127d17bde7e72ae881265eafa1
komstyle <59528384+komstyle@users.noreply.github.com>
2020-02-11 20:45:17 +0900

    devise-user1

--- db/migrate/20200211112010_devise_create_users.rb
+++ db/migrate/20200211112010_devise_create_users.rb
@@ -4,6 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
+      t.string :first_furigana,     null: false
+      t.string :last_furigana,      null: false
+      t.date :birthday,             null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
