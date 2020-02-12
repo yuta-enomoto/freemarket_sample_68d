60dc392b464032a964dfa92c5dae95bb0314bebc
yuta-enomoto <yuta.eno04@gmail.com>
2020-02-13 00:40:45 +0900

    JSを実装

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -1,15 +1,19 @@
 window.addEventListener('DOMContentLoaded', function(){
-
   $('.tab-link').on('click', function() {
     $('.mypage-item-list.tab-pane').removeClass("active");
     $($(this).attr("href")).addClass("active");
-    
+  
+    $('.is-active').removeClass('is-active');
+    $(this).parent().parent().addClass('is-active');
+      return false;
+  });
 
-    $('.tab-link').removeClass('is-active');
-    $(this).addClass('is-active');
+  $('.tab-link-2').on('click', function() {
+    $('.mypage-item-list.tab-pane').removeClass("is-active-now");
+    $($(this).attr("href")).addClass("is-active-now");
+  
+    $('.active-now').removeClass('active-now');
+    $(this).parent().parent().addClass('active-now');
       return false;
   });
 });
-
-
-
