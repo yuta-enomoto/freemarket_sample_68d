c0c1ff67791af806bfbf048ecd997a18994f08e1
ayakamaruyama <ayaka.aaa0624@docomo.ne.jp>
2020-02-13 21:47:01 +0900

    コントローラー編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+  root 'application#index'
   
-  devise_for :users
+  devise_for :users,controllers: {
+    registrations:'users/registrations',
+    sessions: 'users/sessions'
+  }
+  
 end
  
