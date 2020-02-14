04347b72eacf93c466d6b247077858e95da7b534
yuta-enomoto <yuta.eno04@gmail.com>
2020-02-15 02:05:16 +0900

    ハッシュロケットを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # root 'users#show'
-  root 'home#index'
+  root 'users#show'
+  # root 'home#index'
   resources :users, only: [:show]
   resources :items, only: [:new]
   devise_for :users
