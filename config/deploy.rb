769a216a961e7bea8838e2dc62919299a7d9d879
komstyle <59528384+komstyle@users.noreply.github.com>
2020-02-15 17:20:21 +0900

    fix/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,11 +8,18 @@ set :repo_url, "git@github.com:komstyle/freemarket_sample_68d.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
-
+set :rbenv_ruby, '2.5.1'
+set :rbenv_custom_path, '/root/.rbenv'
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/furima-68d.pem'] 
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
