1268e00d34043bd4f3ebea4860e2fc17431fb92c
komstyle <59528384+komstyle@users.noreply.github.com>
2020-02-16 11:19:34 +0900

    fix_9/deploy.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,23 +1,23 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-# app_path = File.expand_path('../../../', __FILE__)
-RAILS_ROOT = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
+
 #アプリケーションサーバの性能を決定する
 worker_processes 1
-ENV['BUNDLE_GEMFILE'] = RAILS_ROOT + "/Gemfile"
+
 #アプリケーションの設置されているディレクトリを指定
-working_directory RAILS_ROOT
+working_directory "#{app_path}/current"
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{RAILS_ROOT}/shared/tmp/pids/unicorn.pid"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen "#{RAILS_ROOT}/shared/tmp/sockets/unicorn.sock"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
-stderr_path "#{RAILS_ROOT}/shared/log/unicorn.stderr.log"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 
 #通常のログを記録するファイルを指定
-stdout_path "#{RAILS_ROOT}/shared/log/unicorn.stdout.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
