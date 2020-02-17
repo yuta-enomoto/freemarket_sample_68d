166776999fd24ed38cfe02d6bfade38b6c22aa73
fjt-kodai <fjt.kodai@gmail.com>
2020-02-17 12:07:47 +0900

    サンプルデータを取り込むためのseedファイルを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,89 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+CSV.foreach('db/sample_data/brands.csv', headers: true) do |row|
+  Brand.create(
+    id: row['id'],
+    name: row['name']
+  )
+end
+
+CSV.foreach('db/sample_data/categories.csv', headers: true) do |row|
+  Category.create(
+    id: row['id'],
+    name: row['name'],
+    ancestry: row['ancestry']
+  )
+end
+
+CSV.foreach('db/sample_data/users.csv', headers: true) do |row|
+  User.create(
+    id: row['id'],
+    nickname: row['nickname'],
+    first_name: row['first_name'],
+    last_name: row['last_name'],
+    first_furigana: row['first_furigana'],
+    last_furigana: row['last_furigana'],
+    birthday: row['birthday'],
+    email: row['email'],
+    password: row['password']
+  )
+end
+
+CSV.foreach('db/sample_data/credit_cards.csv', headers: true) do |row|
+  CreditCard.create(
+    id: row['id'],
+    customer_id: row['customer_id'],
+    card_id: row['card_id'],
+    user_id: row['user_id']
+  )
+end
+
+CSV.foreach('db/sample_data/address.csv', headers: true) do |row|
+  Address.create(
+    id: row['id'],
+    postal_code: row['postal_code'],
+    prefecture_id: row['prefecture_id'],
+    address1: row['address1'],
+    address2: row['address2'],
+    building_name: row['building_name'],
+    phone_number: row['phone_number'],
+    user_id: row['user_id']
+  )
+end
+
+CSV.foreach('db/sample_data/items.csv', headers: true) do |row|
+  Item.create(
+    id: row['id'],
+    name: row['name'],
+    description: row['description'],
+    price: row['price'],
+    stock_status: row['stock_status'],
+    condition_id: row['condirion_id'],
+    shipping_fee_who: row['shipping_fee_who'],
+    prefecture_id: row['prefecture_id'],
+    shipping_days_id: row['shipping_days'],
+    user_id: row['shipping_days'],
+    brand_id: row['shipping_days'],
+    category_id: row['category_id']
+  )
+end
+
+CSV.foreach('db/sample_data/item_images.csv', headers: true) do |row|
+  ItemImage.create(
+    id: row['id'],
+    url: row['url'],
+    item_id: row['item_id']
+  )
+end
+
+CSV.foreach('db/sample_data/orders.csv', headers: true) do |row|
+  Order.create(
+    id: row['id'],
+    user_id: row['user_id:'],
+    item_id: row['item_id']
+  )
+end
