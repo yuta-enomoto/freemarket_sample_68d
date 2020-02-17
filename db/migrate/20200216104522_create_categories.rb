ae1713a69c19cc68c9da39cbac7f921799c333b9
fjt-kodai <fjt.kodai@gmail.com>
2020-02-17 14:52:53 +0900

    ancestryのnull制約を解除

--- db/migrate/20200216104522_create_categories.rb
+++ db/migrate/20200216104522_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
       t.string :name, null: false
-      t.string :ancestry, null: false, index: true
+      t.string :ancestry, index: true
       t.timestamps
     end
   end
