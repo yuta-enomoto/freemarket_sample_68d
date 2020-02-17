1f684b0ffb6337689f3fb17614194aa0b09c89bb
komstyle <59528384+komstyle@users.noreply.github.com>
2020-02-17 15:21:03 +0900

    ユーザー新規登録、ログイン_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  get 'payments/new'
+  get 'payments/create'
+  get 'payments/edit'
+  get 'payments/update'
+  get 'payments/destroy'
+  get 'addresses/new'
+  get 'addresses/create'
+  get 'addresses/edit'
+  get 'addresses/update'
+  get 'addresses/destroy'
   devise_for :users
   root 'home#index'
   resources :items, only: [:show, :new]
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show, :edit, :update]
   resources :oders, only: [:new]
 end
   
