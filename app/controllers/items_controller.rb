fd9b6577f33c32d3d25d02d74545c0077b01d580
t-suke13 <taichanchan13@gmail.com>
2020-02-19 15:28:14 +0900

    商品情報の取得

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,12 @@
 class ItemsController < ApplicationController
   def show
     @item = Item.find(1)
-    @item_image = Item_images.find(1)
+    @item_images = @item.item_images
+
+    
   end
+
+  
 
   def new
   end
