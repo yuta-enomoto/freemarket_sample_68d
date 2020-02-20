e59b13521cbca409484f40626607b7547b3bb648
t-suke13 <taichanchan13@gmail.com>
2020-02-20 18:31:32 +0900

    商品詳細　画像処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
+
   def show
     @item = Item.find(1)
-    @item_images = @item.item_images
-
-    
+    @item_image = @item.item_images[0]
+    @item_images_sub1 = @item.item_images
   end
 
   
