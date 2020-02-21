6d404f55774cd445e7169a8323e1bc5d8a2a5e92
yuta-enomoto <yuta.eno04@gmail.com>
2020-02-21 18:22:54 +0900

    不要なコミットを削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   def show
-    @item = Item.find(1)
+    @item = Item.find(4)
     @item_image = @item.item_images[0]
     @item_images_sub1 = @item.item_images
   end
