cb548fec79bb6f3ae766e62ccda6d6edcada9b95
ayakamaruyama <ayaka.aaa0624@docomo.ne.jp>
2020-02-21 14:52:13 +0900

    商品一覧表示hamlの日本語記述修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
 
   def index
-    @items_category = Item.includes(:user).where(stock_status:"1",category_id:"1")
-    @items_brand = Item.includes(:user).where(stock_status:"1",brand_id:"1",)
+    @items_category = Item.includes(:user).where(stock_status:"1",category_id:"1").order("created_at DESC")
+    @items_brand = Item.includes(:user).where(stock_status:"1",brand_id:"1",).order("created_at DESC")
     
   end
 
