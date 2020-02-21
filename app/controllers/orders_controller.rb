c1868a599a1d4c15f8e3a931e37185490c6c414b
t-suke13 <taichanchan13@gmail.com>
2020-02-21 18:43:04 +0900

    アノテーションコメント形式に修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   def new
-    @item = Item.find(1) #params[:id]
+    @item = Item.find(1) #TODO 仮置き「TOPページ完成後に修正→params[:id]」
     @item_image = @item.item_images[0].url
   end
 end
