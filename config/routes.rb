d89dee3934824a160452efec31473f236e1efffa
fjt-kodai <fjt.kodai@gmail.com>
2020-02-21 12:21:38 +0900

    ルーティングの誤字を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root 'home#index'
+
   resources :users, only: [:show]
-  resources :oders, only: [:new]
+  resources :orders, only: [:new]
   resources :items, only: [:show, :new, :create]
+
 end
   
