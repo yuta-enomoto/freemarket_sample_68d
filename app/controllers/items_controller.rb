5452e428f66ec087e12596d5f3ee539e028753d3
fjt-kodai <fjt.kodai@gmail.com>
2020-02-21 12:22:59 +0900

    多階層プルダウンの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,21 +1,31 @@
 class ItemsController < ApplicationController
-  def show
-  end
 
   def new
     @item = Item.new
     @item.item_images.new
+    @parent_categories = Category.where(ancestry: nil)
+
+    respond_to do |format|
+      format.html
+      format.json {@child_categories = Category.find(params[:category_id]).children}
+    end
   end
 
-  def create
-    # binding.pry
+  # def get_categories
+  #   @child_categories = Category.find(params[:category_id]).children
+  # end
 
+  def create
     @item = Item.new(item_params)
+
     if @item.save
       redirect_to root_path
     else
       render :new
     end
+  end
+
+  def show
   end
 
   private
