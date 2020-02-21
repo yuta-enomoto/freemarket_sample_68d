58f08853c12751f50f8f5ae40e46b7825e962f10
ayakamaruyama <ayaka.aaa0624@docomo.ne.jp>
2020-02-21 11:59:52 +0900

    インスタンス変数の変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-    @items = Item.includes(:user).where(stock_status:"1",category_id:"1")
-    @_items = Item.includes(:user).where(stock_status:"1",brand_id:"1",)
-    
-   
-    
+    @items_category = Item.includes(:user).where(stock_status:"1",category_id:"1")
+    @items_brand = Item.includes(:user).where(stock_status:"1",brand_id:"1",)
     
   end
 
 end
-
-
-# Classmate.where(class_id: 2, club_name: 'tennis')
-# Classmate.where(class_id: 2).where(club_name: 'tennis')
