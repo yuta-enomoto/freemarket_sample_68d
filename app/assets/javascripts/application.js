b30dadc58d9f1efd33fc3433b6e397536088c14d
fjt-kodai <fjt.kodai@gmail.com>
2020-02-21 12:21:06 +0900

    「application.js」で「= require_tree .」を一番下に移動

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require_tree .
 //= require jquery
 //= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
