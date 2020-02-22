0e687924f4e35ff3ab6b89274053e98f5ae991d8
komstyle <59528384+komstyle@users.noreply.github.com>
2020-02-22 17:40:54 +0900

    fix_3

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -23,7 +23,7 @@ document.addEventListener(
               $('<input type="hidden" name="payjp-token">').val(response.id)
             ); //取得したトークンを送信できる状態にします
             document.inputForm.submit();
-            alert("登録が完了しました"); //確認用
+            alert("登録が完了しました。"); //確認用
           } else {
             alert("カード情報が正しくありません。"); //確認用
           }
