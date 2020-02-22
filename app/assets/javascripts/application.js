16dfa70f34e29c4fd4f6ed0bfb2c9f0044457dd1
komstyle <59528384+komstyle@users.noreply.github.com>
2020-02-22 12:43:12 +0900

    fix_2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
-//= require jquery_ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
 
