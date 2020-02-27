74a3adc5d802a7e1dae3ae6699a6c0b8c7c71371
yuta-enomoto <yuta.eno04@gmail.com>
2020-02-27 18:13:52 +0900

    インデントを修正

--- app/assets/javascripts/item-images.js
+++ app/assets/javascripts/item-images.js
@@ -7,7 +7,7 @@ $(document).on('turbolinks:load', function(){
                       <img src="" alt="preview">
                     </div>
                     <div class="lower-box">
-											<div class="update-box", id ="item_item_images_attributes_${count}_url">
+                      <div class="update-box", id ="item_item_images_attributes_${count}_url">
                         <span class="edit_btn">編集</span>
                       </div>
                       <div class="delete-box", id="delete_btn_${count}" "item_item_images_attributes_${count}__destroy"}>
@@ -49,56 +49,56 @@ $(document).on('turbolinks:load', function(){
       }
       if (count == 9) {
         $('.label-content').css('width', 124)
-			}
-		}
-		//商品画像編集フォーム===========================================================================
-		function editForm() {
-			$('.preview-box').each(function(index, box){
-				$(box).attr('id', `preview-box__${index}`);
-			})
-			$('.update-box').each(function(index, box){
-				$(box).attr('id', `item_item_images_attributes_${index}_url`);
-			})
-			$('.delete-box').each(function(index, box){
-				$(box).attr('id', `item_item_images_attributes_${index}__destroy`);
-			})
-			var count = $('.preview-box').length;
-			if (count == 10) {
-				$('.label-content').hide();
-			}
-			$(document).on('click', '.delete-box', function() {
-				var delId = $(this).attr('id')
-				$(`input#${delId}`).prop('checked', true);        
-			})
-		}
+      }
+    }
+    //商品画像編集フォーム===========================================================================
+    function editForm() {
+      $('.preview-box').each(function(index, box){
+        $(box).attr('id', `preview-box__${index}`);
+      })
+      $('.update-box').each(function(index, box){
+        $(box).attr('id', `item_item_images_attributes_${index}_url`);
+      })
+      $('.delete-box').each(function(index, box){
+        $(box).attr('id', `item_item_images_attributes_${index}__destroy`);
+      })
+      var count = $('.preview-box').length;
+      if (count == 10) {
+        $('.label-content').hide();
+      }
+      $(document).on('click', '.delete-box', function() {
+        var delId = $(this).attr('id')
+        $(`input#${delId}`).prop('checked', true);        
+      })
+    }
 
-		// 投稿編集時=============================================================================
-		if (window.location.href.match(/\/items\/\d+\/edit/)){
+    // 投稿編集時=============================================================================
+    if (window.location.href.match(/\/items\/\d+\/edit/)){
       setLabel();
-			editForm();
-		}
-		// エラーメッセージの画面でURLが変わるので、それに対応===========================================
-		if (window.location.href.match(/\/items\/\d+/)){
-			setLabel();
-			editForm();
-		}
+      editForm();
+    }
+    // エラーメッセージの画面でURLが変わるので、それに対応===========================================
+    if (window.location.href.match(/\/items\/\d+/)){
+      setLabel();
+      editForm();
+    }
 
-		//画像を差し替える時=============================================================================
-		$(document).on('click', '.update-box', function() {
-			var updId = $(this).attr('id')
-			$(`input#${updId}`).trigger('click');      
-		})
+    //画像を差し替える時=============================================================================
+    $(document).on('click', '.update-box', function() {
+      var updId = $(this).attr('id')
+      $(`input#${updId}`).trigger('click');      
+    })
     
-		//画像を追加する時=============================================================================
+    //画像を追加する時=============================================================================
     $(document).on('change', '.hidden-field', function() {
       setLabel();
-			var id = $(this).attr('id').replace(/[^0-9]/g, '');
-			$(`input#item_item_images_attributes_${id}__destroy`).prop('checked', false);
+      var id = $(this).attr('id').replace(/[^0-9]/g, '');
+      $(`input#item_item_images_attributes_${id}__destroy`).prop('checked', false);
 
       var file = this.files[0];
       var reader = new FileReader();
-			reader.readAsDataURL(file);
-			
+      reader.readAsDataURL(file);
+    
       reader.onload = function() {
         var image = this.result;
         if ($(`#preview-box__${id}`).length == 0) {
@@ -112,66 +112,66 @@ $(document).on('turbolinks:load', function(){
         }
 
         setLabel();				
-				var id_add = [];
-				$('.preview-box').each(function(index, box){
-					id_add[index] = $(box).attr('id').replace(/[^0-9]/g, '');
-				});
+        var id_add = [];
+        $('.preview-box').each(function(index, box){
+          id_add[index] = $(box).attr('id').replace(/[^0-9]/g, '');
+        });
 
-				var x = $('.preview-box').length
-				for(var index=0; index<x+1; index++){
-					var y = 0
+        var x = $('.preview-box').length
+        for(var index=0; index<x+1; index++){
+          var y = 0
 
-					id_add.map(function(num){
-						if (index == num) {
-							y = 1
-							return false;
-						}
-					});
+          id_add.map(function(num){
+            if (index == num) {
+              y = 1
+              return false;
+            }
+          });
 
-					if (y==0){
-						$('.label-box').attr({id: `label-box--${index}`,for: `item_item_images_attributes_${index}_url`});
-						return false;
-					}
-				}
+          if (y==0){
+            $('.label-box').attr({id: `label-box--${index}`,for: `item_item_images_attributes_${index}_url`});
+            return false;
+          }
+        }
       }
-		});
-		
+    });
+
   //削除ボタンを押した時=============================================================================
     $(document).on('click', '.delete-box', function() {
-			setLabel();
+      setLabel();
       var id = $(this).attr('id').replace(/[^0-9]/g, '');
       $(`#preview-box__${id}`).remove();
 
-			var id_add2 = [];
-			$('.preview-box').each(function(index, box){
-				id_add2[index] = $(box).attr('id').replace(/[^0-9]/g, '');
-			});
+      var id_add2 = [];
+      $('.preview-box').each(function(index, box){
+        id_add2[index] = $(box).attr('id').replace(/[^0-9]/g, '');
+      });
 
-			$('.preview-box').each(function(index){
-				var y = 0
+      $('.preview-box').each(function(index){
+        var y = 0
 
-				id_add2.forEach(function(num){
-					if (index == num) {
-						y = 1
-						return false;
-					}
-				});
+        id_add2.forEach(function(num){
+          if (index == num) {
+            y = 1
+            return false;
+          }
+        });
 
-				if (y==0){
-					$('.label-box').attr({id: `label-box--${index}`,for: `item_item_images_attributes_${index}_url`});
-					return false;
-				}
-			});
+        if (y==0){
+          $('.label-box').attr({id: `label-box--${index}`,for: `item_item_images_attributes_${index}_url`});
+          return false;
+        }
+      });
 
-			//削除ボタンを押した時ファイルフィールドの中身を消す===============================
-			$(`#item_item_images_attributes_${id}_url`).val("");
-			var count = $('.preview-box').length;
+      //削除ボタンを押した時ファイルフィールドの中身を消す===============================
+      $(`#item_item_images_attributes_${id}_url`).val("");
+      var count = $('.preview-box').length;
 
-			//10個めが消されたらラベルを表示===============================================
-			setLabel();
-			if (count == 9) {
-				$('.label-content').show();
-			};
+      //10個めが消されたらラベルを表示===============================================
+      setLabel();
+      if (count == 9) {
+        $('.label-content').show();
+      };
     });
   });
 });
