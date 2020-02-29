aa489b1d34b6eff4d85cc3ab9ffaab346b1eb44c
fjt-kodai <fjt.kodai@gmail.com>
2020-02-29 21:40:05 +0900

    sns認証用のdbを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_16_104527) do
+ActiveRecord::Schema.define(version: 2020_02_25_083330) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -86,6 +86,15 @@ ActiveRecord::Schema.define(version: 2020_02_16_104527) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "first_name", null: false
@@ -112,4 +121,5 @@ ActiveRecord::Schema.define(version: 2020_02_16_104527) do
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
+  add_foreign_key "sns_credentials", "users"
 end
