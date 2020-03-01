ec9931758a0bf8336102c8e69c1b18f5fa20999a
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-01 21:43:03 +0900

    空文字検索の場合に対応

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,13 @@
 class SearchesController < ApplicationController
 
   def detail_search
-    @search_item = Item.ransack(params[:q]) 
-    @items = @search_item.result.page(params[:page])
-    @search = params[:q][:name_cont]
+    if params[:q][:name_cont].empty?
+
+    else
+      @search_item = Item.ransack(params[:q])
+      @items = @search_item.result.page(params[:page])
+      @search = params[:q][:name_cont]
+    end
   end
 
 end
