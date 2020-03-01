2e32c764916b1d9376bbc6ebce0ad73749e5ef71
komstyle <59528384+komstyle@users.noreply.github.com>
2020-03-01 17:48:39 +0900

    fix_7

--- app/helpers/Pickup_helper.rb
+++
@@ -1,9 +0,0 @@
-module PickupHelper
-  def picｋ_up
-    brand = Brand.all.sample
-    parent = Category.where(ancestry: nil)
-    picｋ_up = parent.sample
-    category_ary = Category.where("id = ? or ancestry = ? or ancestry LIKE(?)", picｋ_up.id, picｋ_up.id, "#{picｋ_up.id}/%")
-    return brand, parent, picｋ_up, category_ary
-  end
-end
