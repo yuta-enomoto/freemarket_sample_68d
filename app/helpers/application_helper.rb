ca4016f8d63c83e4409eaccdb66384edda3b9457
komstyle <59528384+komstyle@users.noreply.github.com>
2020-03-01 17:35:14 +0900

    fix_4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,12 +17,4 @@ module ApplicationHelper
     end
     return exp_year
   end
-
-  def picｋ_up
-    brand = Brand.all.sample
-    parent = Category.where(ancestry: nil)
-    picｋ_up = parent.sample
-    category_ary = Category.where("id = ? or ancestry = ? or ancestry LIKE(?)", picｋ_up.id, picｋ_up.id, "#{picｋ_up.id}/%")
-    return brand, parent, picｋ_up, category_ary
-  end
 end
