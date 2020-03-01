97e204375abdacdb407470362ce6428b5f92603f
komstyle <59528384+komstyle@users.noreply.github.com>
2020-03-01 17:47:57 +0900

    fix_6

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,13 @@
 class HomeController < ApplicationController
 
   def index
-    @brand, @parent_categories, @picｋ_up_category, category_list = picｋ_up
+    @brand, @parent_categories, @picｋ_up_category, category_list = pick_up
     @items_category = Item.includes(:user).where(stock_status: true, category_id: category_list).date_desc
     @items_brand = Item.includes(:user).where(stock_status: true, brand_id: @brand.id).date_desc
-
-    # @items_category = Item.includes(:user).where(stock_status:"1",category_id:"1").date_desc
-    # @items_brand = Item.includes(:user).where(stock_status:"1",brand_id:"1",).date_desc
-    # @parent_categories = Category.where(ancestry: nil)
   end
 
   private
-  def picｋ_up
+  def pick_up
     brand = Brand.all.sample
     parent = Category.where(ancestry: nil)
     picｋ_up = parent.sample
