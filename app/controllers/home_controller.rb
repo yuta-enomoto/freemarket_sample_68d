8b74b7c9aa997761aa9b7140098089c54939c687
komstyle <59528384+komstyle@users.noreply.github.com>
2020-03-01 17:17:14 +0900

    fix_1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,13 @@
 class HomeController < ApplicationController
-  include PickupHelper
+  # include PickupHelper
 
   def index
-    @brand, @parent_categories, @picｋ_up_category, category_list = picｋ_up
-    @items_category = Item.includes(:user).where(stock_status: true, category_id: category_list).date_desc
-    @items_brand = Item.includes(:user).where(stock_status: true, brand_id: @brand.id).date_desc
+    # @brand, @parent_categories, @picｋ_up_category, category_list = picｋ_up
+    # @items_category = Item.includes(:user).where(stock_status: true, category_id: category_list).date_desc
+    # @items_brand = Item.includes(:user).where(stock_status: true, brand_id: @brand.id).date_desc
+
+    @items_category = Item.includes(:user).where(stock_status:"1",category_id:"1").date_desc
+    @items_brand = Item.includes(:user).where(stock_status:"1",brand_id:"1",).date_desc
+    @parent_categories = Category.where(ancestry: nil)
   end
 end 
