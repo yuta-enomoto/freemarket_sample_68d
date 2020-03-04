de554196f6b9f2ffab05576eb212e4d2181661d4
komstyle <59528384+komstyle@users.noreply.github.com>
2020-03-04 09:44:58 +0900

    fix1

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,11 +1,13 @@
-class Brand < ActiveHash::Base
+class Brand < ApplicationRecord
+  has_many :items
 
-  self.data = [
-    {id: 1, name: 'シャネル'},
-    {id: 2, name: 'ナイキ'}, 
-    {id: 3, name: 'ルイ ヴィトン'},
-    {id: 4, name: 'シュプリーム'},
-    {id: 5, name: 'アディダス'}, 
-  ]
+  validates :name, presence: true
+  # self.data = [
+  #   {id: 1, name: 'シャネル'},
+  #   {id: 2, name: 'ナイキ'}, 
+  #   {id: 3, name: 'ルイ ヴィトン'},
+  #   {id: 4, name: 'シュプリーム'},
+  #   {id: 5, name: 'アディダス'}, 
+  # ]
 
 end
